e2ad46705ff1552145ffb650132021470a4078ec
Radoslaw Matlak <radek1999@onet.com.pl>
2019-07-31 15:53:17 +0200

    Updates

---
+++ precache-manifest.265c8782c2871e6ab6b4cc89128f60f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d398177f6fa080212bcf7d64d2a83e13",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "c07f5d374f69a1400732",
+    "url": "/WeatherApp/static/css/main.bfccf66e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/WeatherApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c07f5d374f69a1400732",
+    "url": "/WeatherApp/static/js/main.3f188769.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  }
+]);
